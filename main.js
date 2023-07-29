111f7ecd1c64f5df86f0cbfb0a9195e51d1de61a
GRRTYR <140865514+GRRTYR@users.noreply.github.com>
2023-07-29 10:59:21 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+console.log("修改：\n %c 三岁の小窝🍋 %c by 三岁 | www.sansui.vip ", "color:#444;background:#eee;padding:5px 0;", "color:#F8F8FF;background:#F4A7B9;padding:5px 0;");
+console.log("原作：\n %c Made with love %c by AyagawaSeirin | www.seir.in ", "color:#444;background:#eee;padding:5px 0;", "color:#F8F8FF;background:#F4A7B9;padding:5px 0;");
